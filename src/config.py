74959f6e325f47c458ae34335da654ccf383427d
iamaziz <iamaziz.alto@gmail.com>
2022-05-16 22:16:06 -0400

    title

--- src/config.py
+++ src/config.py
@@ -5,7 +5,9 @@ def APP_PAGE_HEADER():
     st.set_page_config(
         page_title="U.S. Patent", page_icon="🔬", layout="wide", initial_sidebar_state="collapsed"
     )
-
+    st.markdown(
+        "### [U.S. Patent Phrase to Phrase Matching](https://www.kaggle.com/competitions/us-patent-phrase-to-phrase-matching)",
+        unsafe_allow_html=True)
     hide_streamlit_style = """
                     <style>
                     #MainMenu {visibility: hidden;}
